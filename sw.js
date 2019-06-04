448ac237ffb2ccdb43523d797946d6dcc61345ca
Agnieszka Bocian <agn.bocian@gmail.com>
2019-06-04 16:48:31 +0200

    Feat: Add a service worker file

---
+++ sw.js
@@ -0,0 +1,78 @@
+'use strict';
+
+const staticCacheName = 'restaurant-reviews-v1';
+var allCaches = [staticCacheName];
+
+const urlsCached = [
+  '.',
+  'index.html',
+  'restaurant.html',
+  'css/styles.css',
+  'js/dbhelper.js',
+  'js/main.js',
+  'js/restaurant_info.js',
+  'data/restaurants.json',
+  'img/',
+  'img/1.jpg',
+  'img/2.jpg',
+  'img/3.jpg',
+  'img/4.jpg',
+  'img/5.jpg',
+  'img/6.jpg',
+  'img/7.jpg',
+  'img/8.jpg',
+  'img/9.jpg',
+  'img/10.jpg'
+];
+
+
+/* Caching files */
+
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open(staticCacheName).then(function (cache) {
+            return cache.addAll(urlsCached);
+        })
+    );
+});
+
+/* Deleting old catches */
+
+
+self.addEventListener('activate', function (event) {
+    event.waitUntil(caches.keys().then(function (cacheNames) {
+        return Promise.all(cacheNames.filter(function (cacheName) {
+            return cacheName.startsWith('restaurant-reviews-') && !allCaches.includes(cacheName);
+        }).map(function (cacheName) {
+            return caches['delete'](cacheName);
+        }));
+    }));
+});
+
+
+
+/* Fetching cached files */
+
+self.addEventListener('fetch', function (event) {
+
+    var requestUrl = new URL(event.request.url);
+
+    if (requestUrl.origin === location.origin) {
+        if (requestUrl.pathname.startsWith('/restaurant.html')) {
+            event.respondWith(caches.match('restaurant.html'));
+        }
+        /* Fetching cached images */
+        if (requestUrl.pathname.startsWith('/img/')) {
+                /* Replacing url of compressed images with url of cached full resolution images */
+                var imgUrl = requestUrl.pathname.replace(/[0-9]00w-/, '');
+                
+                event.respondWith(caches.match(imgUrl));
+        }
+        else {
+            event.respondWith(caches.match(event.request).then(function (response) {
+                return response || fetch(event.request);
+            }));
+        }
+    }
+
+});
